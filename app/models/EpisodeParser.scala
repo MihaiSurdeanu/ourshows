c74e471d6863261f392af68e32bc789a40a1664e
Mihai Surdeanu <surdeanu@gmail.com>
2016-11-04 21:02:04 -0700

    Added CC parser

--- app/models/EpisodeParser.scala
+++ app/models/EpisodeParser.scala
@@ -16,11 +16,13 @@ object EpisodeParser {
     if(url.getHost.contains("abc.go.com")) {
       parseABCEpisodes(url, html)
     } else if(url.getHost.contains("cbs.com")) {
-      // TODO
-      List.empty
+      parseCBSEpisodes(url, html)
     } else if(url.getHost.contains("fox.com")) {
-      // TODO
-      List.empty
+      parseFoxEpisodes(url, html)
+    } else if(url.getHost.contains("nbc.com")) {
+      parseNBCEpisodes(url, html)
+    } else if(url.getHost.contains("cc.com")) {
+      parseCCEpisodes(url, html)
     } else {
       throw new RuntimeException(s"ERROR: Unknown domain for URL: $url!")
     }
@@ -29,21 +31,55 @@ object EpisodeParser {
   def parseABCEpisodes(url:URL, html:String):List[Episode] = {
     val episodes = new ListBuffer[Episode]
     val matcher = ABC_EPISODE.matcher(html)
-    //println(s"Trying to match: ${ABC_EPISODE.toString}")
-    //println(html)
     var count = 0
-    while(matcher.find() && count < MAX_EPISODES) {
+    while(matcher.find()) {
       val text = matcher.group()
       val episodeUrl = url.getProtocol + "://" + url.getHost + matcher.group(1)
       val seasonNumber = matcher.group(2).toInt
       val episodeNumber = matcher.group(3).toInt
       val episodeTitle = matcher.group(4)
-      //println(s"Found URL [$url] and title [$episode] in text [$text]")
       episodes += new Episode(episodeTitle, episodeUrl, seasonNumber, episodeNumber)
       count += 1
     }
-    //println(s"Found $count episodes.")
-    episodes.toList.sortBy(0 - _.number)
+    sortAndTake(episodes)
+  }
+
+  def parseCBSEpisodes(url:URL, html:String):List[Episode] = {
+    // TODO: this is tricky, the carousel with episodes is hidden
+    List.empty
+  }
+
+  def parseFoxEpisodes(url:URL, html:String):List[Episode] = {
+    // TODO
+    List.empty
+  }
+
+  def parseNBCEpisodes(url:URL, html:String):List[Episode] = {
+    // TODO
+    List.empty
+  }
+
+  def parseCCEpisodes(url:URL, html:String):List[Episode] = {
+    val episodes = new ListBuffer[Episode]
+    val matcher = CC_EPISODE.matcher(html)
+    var count = 0
+    while(matcher.find()) {
+      val text = matcher.group()
+      val episodeUrl = matcher.group(1)
+      val episodeTitle = matcher.group(2)
+      val urlMatcher = CC_URL.matcher(episodeUrl)
+      if(urlMatcher.matches()) {
+        val seasonNumber = urlMatcher.group(1).toInt
+        val episodeNumber = urlMatcher.group(2).toInt
+        episodes += new Episode(episodeTitle, episodeUrl, seasonNumber, episodeNumber)
+        count += 1
+      }
+    }
+    sortAndTake(episodes)
+  }
+
+  def sortAndTake(episodes:Seq[Episode]):List[Episode] = {
+    episodes.toList.sortBy(x => 0 - (x.season * 100 + x.number)).take(MAX_EPISODES)
   }
 
   def main(args:Array[String]): Unit = {
@@ -53,8 +89,15 @@ object EpisodeParser {
 
   val MAX_EPISODES = 5 // max number of episodes to show per show
 
+  // ABC:
   // 1: URL, 2: season number, 3: episode number, 4: title
   val ABC_EPISODE = Pattern.compile("""<a\s+href="([^"]+)"[^>]+>\s*<span\s+class="season-number\s*light">S(\d+)\s*</span>\s+<span\s+class="episode-number">E(\d+)\s+</span>([^><]+)</a>""", Pattern.CASE_INSENSITIVE)
+
+  // Comedy Central:
+  // 1. URL, 2: title
+  val CC_EPISODE = Pattern.compile("""<a\s+class="carouselseo"\s+href="([^"]+)">([^><]+)</a>""", Pattern.CASE_INSENSITIVE)
+  // 1: season number, 2: episode number
+  val CC_URL = Pattern.compile("""http://southpark.cc.com/full-episodes/s(\d+)e(\d+)-[a-z0-9\-]+""", Pattern.CASE_INSENSITIVE)
 }
 
 
